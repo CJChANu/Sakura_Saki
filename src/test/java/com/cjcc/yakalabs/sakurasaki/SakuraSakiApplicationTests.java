17177b77fdf9a240d0128aab2b1febe6f530f21d
CJ_ChANu <cjchanux@gmail.com>
2026-03-16 17:25:39 +0530

    Initial Sakura Saki Spring Boot setup

---
+++ src/test/java/com/cjcc/yakalabs/sakurasaki/SakuraSakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjcc.yakalabs.sakurasaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakuraSakiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
