66c94f73d792963cea4d35c6982ca2e844d63eeb
IT25103879 <it25103879@my.sliit.lk>
2026-05-15 23:01:24 +0530

    Creat HairService , NailService , SkinService classes

---
+++ src/main/java/com/cjcc/yakalabs/sakurasaki/model/HairService.java
@@ -0,0 +1,29 @@
+package com.example.beautysalonbookingsystem.model;
+
+
+public class HairService extends SalonService {
+    private String hairType;
+
+    public HairService() {
+        super();
+    }
+
+    public HairService(String serviceId, String serviceName, int duration, double price,
+                       String description, boolean available, String hairType) {
+        super(serviceId, serviceName, "Hair", duration, price, description, available);
+        this.hairType = hairType;
+    }
+
+    public String getHairType() {
+        return hairType;
+    }
+
+    public void setHairType(String hairType) {
+        this.hairType = hairType;
+    }
+
+    @Override
+    public String getCategoryDetails() {
+        return "Hair service for " + hairType + " hair type";
+    }
+}
