7dd0c5c07476e26090f73192ec8253f4d1571838
P T J Thakshila <it25100393@my.sliit.lk>
2026-05-15 09:37:16 +0530

    Add PackageController

---
+++ src/main/java/com/cjcc/yakalabs/sakurasaki/controller/PackageController.java
@@ -0,0 +1,54 @@
+package com.example.beautysalonbookingsystem.controller;
+
+
+import com.example.beautysalonbookingsystem.model.ServicePackage;
+import com.example.beautysalonbookingsystem.service.ServiceManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/packages")
+public class PackageController {
+
+    @Autowired
+    private ServiceManagementService serviceManagementService;
+
+    @GetMapping
+    public String viewAllPackages(Model model) {
+        model.addAttribute("packages", serviceManagementService.getAllPackages());
+        return "package-list";
+    }
+
+    @GetMapping("/add")
+    public String showAddPackageForm(Model model) {
+        model.addAttribute("servicePackage", new ServicePackage());
+        return "add-package";
+    }
+
+    @PostMapping("/add")
+    public String addPackage(@ModelAttribute("servicePackage") ServicePackage servicePackage) {
+        serviceManagementService.addPackage(servicePackage);
+        return "redirect:/packages";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditPackageForm(@PathVariable("id") String id, Model model) {
+        ServicePackage servicePackage = serviceManagementService.getPackageById(id);
+        model.addAttribute("servicePackage", servicePackage);
+        return "edit-package";
+    }
+
+    @PostMapping("/update")
+    public String updatePackage(@ModelAttribute("servicePackage") ServicePackage servicePackage) {
+        serviceManagementService.updatePackage(servicePackage);
+        return "redirect:/packages";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deletePackage(@PathVariable("id") String id) {
+        serviceManagementService.deletePackage(id);
+        return "redirect:/packages";
+    }
+}
